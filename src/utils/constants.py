25196ba2d776133ac9ae732349f6fac4fcd25b8a
Aldy Naufal <114373270+aldyNaufal@users.noreply.github.com>
2024-06-16 20:06:45 +0700

    Add files via upload

---
+++ src/utils/constants.py
@@ -0,0 +1,20 @@
+# list of all possible players (tuple of 3 positions of flap)
+PLAYERS = (
+    # red bird
+    (
+        "assets/sprites/brone-upflap.png",
+        "assets/sprites/brone-midflap.png",
+        "assets/sprites/brone-downflap.png",
+    ),
+)
+
+# list of backgrounds
+BACKGROUNDS = (
+    "assets/sprites/background-day.png",
+    "assets/sprites/background-night.png",
+)
+
+# list of pipes
+PIPES = (
+    "assets/sprites/jam-ub.png",
+)
